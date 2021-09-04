83a53bef9fe4f2d9244d021a8c0f74a40471ce9a
mattgreen91 <mattgreen91@outlook.com>
2021-09-04 18:43:27 +0000

    reupdated json files

--- store/models.py
+++ store/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Type(models.Model):
+class Category(models.Model):
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
@@ -11,7 +11,7 @@ class Type(models.Model):
         return self.friendly_name
 
 
-class Game(models.Model):
+class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
