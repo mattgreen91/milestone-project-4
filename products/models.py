ea71a15f92e8b3527091894a8284ee5a163578f3
mattgreen91 <mattgreen91@outlook.com>
2021-09-09 10:46:51 +0000

    changed columns for smaller screen sizes to show only 1

--- products/models.py
+++ products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 
 class Category(models.Model):
@@ -24,7 +25,7 @@ class Product(models.Model):
     selling_price = models.DecimalField(max_digits=6, decimal_places=2)
     new = models.BooleanField()
     age = models.DecimalField(max_digits=2, decimal_places=0)
-    rating = models.DecimalField(max_digits=1, decimal_places=0, null=True, blank=True)
+    rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)], null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
