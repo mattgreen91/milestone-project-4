f7143d7c7bbdcd24252be786829431325750349d
mattgreen91 <mattgreen91@outlook.com>
2021-10-14 01:25:27 +0000

    created popup menu for are you sure you want to delete?  also removed product image url field, as not using

--- products/models.py
+++ products/models.py
@@ -26,7 +26,6 @@ class Product(models.Model):
     new = models.BooleanField()
     age = models.DecimalField(max_digits=2, decimal_places=0)
     rating = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(5)], null=True, blank=True)
-    image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
