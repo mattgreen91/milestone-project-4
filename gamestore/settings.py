58b073024041a83ecb7d5649ff2c9fead116ca72
mattgreen91 <mattgreen91@outlook.com>
2021-10-15 11:51:13 +0000

    removed secret key and set debug

--- gamestore/settings.py
+++ gamestore/settings.py
@@ -22,10 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pa$rnjz=_lh2a2f+frvq7ii&(liy3gy14ap2eny1#33hpoik%%'
+# key is a custom made django key for use between heroku and the app, can be any key
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['game-store-uk.herokuapp.com', 'localhost']
 
