23f6045d9f51420269a1e0223887d297b0a67646
mattgreen91 <mattgreen91@outlook.com>
2021-10-15 11:19:49 +0000

    created procfile and started deploying to heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn gamestore.wsgi:application
--- gamestore/settings.py
+++ gamestore/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +27,7 @@ SECRET_KEY = 'django-insecure-pa$rnjz=_lh2a2f+frvq7ii&(liy3gy14ap2eny1#33hpoik%%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['game-store-uk.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -123,12 +124,17 @@ WSGI_APPLICATION = 'gamestore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 
 # Password validation
