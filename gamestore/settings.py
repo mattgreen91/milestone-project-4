cca899fd4dcc3304c8359ecaf7f7926ff6315fe3
mattgreen91 <mattgreen91@outlook.com>
2021-10-15 21:09:19 +0000

    tidied up settings.py

--- gamestore/settings.py
+++ gamestore/settings.py
@@ -106,8 +106,6 @@ AUTHENTICATION_BACKENDS = [
 
 SITE_ID = 1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 # settings for allauth
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
@@ -181,6 +179,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
+     # Cache control	
+    AWS_S3_OBJECT_PARAMETERS = {	
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',	
+        'CacheControl': 'max-age=94608000',	
+    }
+
     #AWS Bucket config
     AWS_STORAGE_BUCKET_NAME = 'game-store-uk'
     AWS_S3_REGION_NAME = 'eu-west-2'
@@ -211,4 +215,16 @@ STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'gamestore@programmer.net'
+
+
+if 'DEVELOPMENT' in os.environ:	
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'	
+    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'	
+else:	
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'	
+    EMAIL_USE_TLS = True	
+    EMAIL_PORT = 587	
+    EMAIL_HOST = 'smtp.gmail.com'	
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')	
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = 'gamestore@programmer.net'
