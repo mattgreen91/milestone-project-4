0313c6f28fc96458d3400dd405adc4f04a798f8c
mattgreen91 <mattgreen91@outlook.com>
2021-10-16 15:19:04 +0000

    updated settings.py to include google allauth

--- gamestore/settings.py
+++ gamestore/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
     'home',
     'products',
     'basket',
@@ -218,7 +219,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:	
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'	
-    DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'	
+    DEFAULT_FROM_EMAIL = 'gamestore@programmer.net'	
 else:	
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'	
     EMAIL_USE_TLS = True	
