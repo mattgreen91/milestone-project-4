7a026fec29a659d03b9c96f391ff501a75079bfe
mattgreen91 <mattgreen91@outlook.com>
2021-11-07 17:49:41 +0000

    added google authenticator to views.py and urls.py

--- home/views.py
+++ home/views.py
@@ -6,3 +6,9 @@ def index(request):
     """ A view to return to index page """
     
     return render(request, 'home/index.html')
+
+
+def google(request):
+    """ A view to return to google authentication page """
+    
+    return render(request, 'home/googlef6bdd563d9a6019b.html')
