261867bd61d8c3464c6ede502c0faadedd622fb8
mattgreen91 <mattgreen91@outlook.com>
2021-11-13 11:56:47 +0000

    removed duplicated rel=stylesheet from base.html

--- checkout/static/checkout/js/stripe.js
+++ checkout/static/checkout/js/stripe.js
@@ -109,7 +109,7 @@ form.addEventListener('submit', function(ev) {
                 card.update({ 'disabled': false});
                 $('#submit-button').attr('disabled', false);
             } else {
-                console.log("Submitted")
+                console.log("Submitted");
                 if (result.paymentIntent.status === 'succeeded') {
                     form.submit();
                 }
@@ -118,5 +118,5 @@ form.addEventListener('submit', function(ev) {
     }).fail(function () {
         // just reload the page, the error will be in django messages
         location.reload();
-    })
+    });
 });
